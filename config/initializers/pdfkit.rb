c46de01f59275ac6741674deb821644faec14f34
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-09-17 13:46:12 +0100

    Positioning labels - ok up to 3 pages

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,3 +1,4 @@
 PDFKit.configure do |config|       
   config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-heroku').to_s if RAILS_ENV == 'production'  
+  config.default_options = {:margin_left => 5, :margin_right => 5, :margin_top => 12, :margin_bottom => 11}
 end
