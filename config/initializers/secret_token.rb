2e4c51886430d47fbc3732ba47c94de628939d4b
Artur Roszczyk <artur.roszczyk@gmail.com>
2010-09-17 02:09:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paypalabels::Application.config.secret_token = 'd2ac4ecafc292c0458ef73978f3cbbf0c4db04472cbf50a8cd5af3147b071626e992b99b618c0ba68a36701eabc6c11cb7b6fa5dbdc08ff3c3869134c2b9d04e'
